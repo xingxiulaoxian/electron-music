7ef3603a4d0afd1d328280b9c75d705c01e78a2c
xingxiulaoxian <991836979@qq.com>
2018-05-07 10:30:39 +0800

    hello world

---
+++ main.js
@@ -0,0 +1,17 @@
+const {app, BrowserWindow} = require('electron')
+  const path = require('path')
+  const url = require('url')
+  
+  function createWindow () {
+    // 创建一个窗口.
+    win = new BrowserWindow({width: 800, height: 600})
+  
+    // 然后加载应用的 index.html。
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
+  }
+  
+  app.on('ready', createWindow)
