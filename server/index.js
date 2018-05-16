689fb54b15ac86e5dbdcee1b026e2e0c719587e5
xingxiulaoxian <991836979@qq.com>
2018-05-16 09:08:45 +0800

    server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('hhhhhhhhh')
+})
+
+const server = app.listen('3000', function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+})
